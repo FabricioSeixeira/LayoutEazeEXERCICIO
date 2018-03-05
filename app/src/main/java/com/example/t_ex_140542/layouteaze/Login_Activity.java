57328f32499219e1e0a632e627e6fa9438d14bd6
t-ex-140542 <fabicio001@outlook.com>
2018-03-05 12:29:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/t_ex_140542/layouteaze/Login_Activity.java
@@ -0,0 +1,13 @@
+package com.example.t_ex_140542.layouteaze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
